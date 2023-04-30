e1ccc0650ef78b4d9214cd18d1ef8504d23f2827
Stephen Parinas <61570582+neilstephen2001@users.noreply.github.com>
2023-05-01 01:13:28 +1200

    flask
    made the buttons redirect to the correct pages when clicked

--- app.py
+++ app.py
@@ -26,12 +26,33 @@ def index():
 
 @app.route('/submit-lastfm', methods=['POST'])
 def submit_lastfm():
+    username = request.form['username']
+    time_period = request.form['time-period']
+    tracks = request.form['tracks']
     return redirect(url_for('results'))
 
-@app.route('/results', methods=['GET'])
+@app.route('/results')
 def results():
-    return 'hi'
+    return render_template('display-results.html')
 
+@app.route('/return-home', methods=['POST'])
+def return_home():
+    if 'export' in request.form:
+        return render_template('create-playlist.html')
+    else:
+        return redirect(url_for('index'))
 
+@app.route('/playlist-create', methods=['POST'])
+def playlist_create():
+    playlist_name = request.form.get('playlist')
+    playlist_description = request.form.get('description')
+    return redirect(url_for('playlist_display'))
+
+@app.route('/playlist-display')
+def playlist_display():
+    embedded_playlist_url = "https://open.spotify.com/embed/playlist/000hqMJogZg18TKJHYsidZ?utm_source=generator"
+    return render_template('display-playlist.html', value = embedded_playlist_url)
+
+    
 if __name__ == '__main__':
     app.run()
--- main.py
+++ main.py
@@ -9,3 +9,10 @@ the last.fm data and creating the Spotify playlist
 import requests
 import json
 import spotipy
+
+def get_top_tracks(username, time_period, tracks):
+    pass
+
+def exceptions(response):
+    print("Exception occurred with status code: ", response.status_code)
+    print("Error: ", response.text)
