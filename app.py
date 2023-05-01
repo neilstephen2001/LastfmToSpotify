39a1d9c40b490d41777de70e3a12b60d0336baf4
Stephen Parinas <61570582+neilstephen2001@users.noreply.github.com>
2023-05-01 19:53:14 +1200

    app
    combined app and main files into one

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ Flask app
 
 from flask import Flask, request, url_for, session, redirect, render_template, jsonify
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy import util
 import spotipy
 import time
 import requests
 import json
-import config, main
+import config
 
-# Flask app
+"""FLASK APP"""
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.secret_key = 'stan_exo'
 
-@app.route('/login')
+@app.route('/')
 def login():
     sp_oauth = SpotifyOAuth(client_id=config.SPOTIFY_ID,
                             client_secret=config.SPOTIFY_SECRET,
@@ -31,19 +32,21 @@ def login():
 
 @app.route('/callback')
 def callback():
-    print('Redirected')
     sp_oauth = SpotifyOAuth(client_id=config.SPOTIFY_ID,
                             client_secret=config.SPOTIFY_SECRET,
                             redirect_uri=config.SPOTIFY_REDIRECT_URI,
                             scope=['playlist-modify-private', 'playlist-modify-public'],
                             cache_path='cache',
                             show_dialog=True)
+    session.clear()
     code = request.args.get('code')
+    print(code)
     token_info = sp_oauth.get_access_token(code)
+    print(token_info)
     session['token_info'] = token_info
     return redirect(url_for('index'))
 
-@app.route('/')
+@app.route('/home')
 def index():
     token_info = session.get('token_info', None)
     if not token_info:
@@ -65,10 +68,10 @@ def submit_lastfm():
 @app.route('/get-data')
 def get_data():
     global track_data
-    track_data = main.get_top_tracks(user = username, period = time_period, limit = tracks)
+    track_data = get_top_tracks(user = username, period = time_period, limit = tracks)
     global song_uri
-    song_uri, artist_list = main.get_song_uri(track_data)
-    main.get_artist_image(artist_list, track_data)
+    song_uri, artist_list = get_song_uri(track_data)
+    get_artist_image(artist_list, track_data)
     return redirect(url_for('results'))
 
 @app.route('/process-data')
@@ -106,7 +109,7 @@ def playlist_create():
         'description': request.form.get('description'),
         'public': False
     })
-    embedded_playlist_url = main.create_playlist(playlist_details, song_uri)
+    embedded_playlist_url = create_playlist(playlist_details, song_uri)
     return redirect(url_for('playlist_display'))
 
 @app.route('/playlist-display')
@@ -117,40 +120,161 @@ def playlist_display():
 def return_home():
     return redirect(url_for('index'))
 
-# other functions
-def create_playlist(playlist_details, uri_list):
-    token_info = spotipy.Spotify(auth=session.get('token_info'))
-    sp = token_info.get('access_token')
+
+"""OTHER FUNCTIONS"""
+
+# Returns the user's top tracks from last.fm
+def get_top_tracks(user, period, limit):
+    headers = {
+        'user-agent': config.USER_AGENT
+    }
+    payload = {
+        'api_key': config.LASTFM_API,
+        'method': "user.getTopTracks",
+        'format': 'json',
+        'user': user,
+        'period': period,
+        'limit': limit
+    }
+    r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+    if r.status_code != 200:
+        exceptions(r)
+    else:
+        track_data = []
+        response = r.json()
+        for track in response['toptracks']['track']:
+            data = {
+                'rank': track['@attr']['rank'],
+                'track_name': track['name'],
+                'playcount': track['playcount'],
+                'url': track['url'],
+                'artist_name': track['artist']['name'],
+            }
+            track_data.append(data)
+        return track_data
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(client_id=config.SPOTIFY_ID,
+                        client_secret=config.SPOTIFY_SECRET,
+                        redirect_uri=config.SPOTIFY_REDIRECT_URI,
+                        scope=['playlist-modify-private', 'playlist-modify-public'])
+
+
+# Returns the Spotify URI for each of the user's top songs
+def get_song_uri(track_data):
+    access_token = session.get('token_info').get('access_token')  
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer { sp }"
+        "Authorization": f"Bearer { access_token }"
     }
-    username = sp.me()['id']
+    uri_list = []
+    artist_list = []
+    for i in range(len(track_data)):
+        item = track_data[i]
+        # remove punctuation as spotify does not recognise it when searching
+        song = remove_punctuation(item['track_name'])
+        artist = remove_punctuation(item['artist_name'])
+
+        url = f"https://api.spotify.com/v1/search?query=track%3A{song}+artist%3A{artist}&type=track&offset=0&limit=5"
+        r = requests.get(url = url, headers = headers)
+
+        if r.status_code != 200:
+            exceptions(r)
+            break
+        else:
+            uri = r.json()['tracks']['items']
+            if not uri:
+                artist_list.append({'id': ""})
+                uri_list.append("")
+            else:
+                # find matching song out of the top 3 results
+                # if none match, pick the top result
+                top_results = 5
+                if len(uri) < 5:
+                    top_results = len(uri)
+                for i in range(top_results):
+                    if len(item['track_name']) == len(uri[i]['name']):
+                        uri_list.append(uri[i]['uri'])
+                        artist_list.append(uri[i]['artists'][0])
+                        break
+                    elif i == 4:
+                        uri_list.append(uri[0]['uri'])
+                        artist_list.append(uri[i]['artists'][0])
+    return uri_list, artist_list
+
+
+# Returns the artist images to display with results
+def get_artist_image(artist_list, track_data):
+    access_token = session.get('token_info').get('access_token')
+    sp = spotipy.Spotify(auth=access_token)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer { access_token }"
+    }
+    for i in range(len(artist_list)):
+        artist_id = artist_list[i]['id']
+        if artist_id != "":
+            url = f"https://api.spotify.com/v1/artists/{artist_id}"
+            r = requests.get(url = url, headers = headers)
+
+            if r.status_code != 200:
+                exceptions(r)
+                break
+            else:
+                track_data[i]['image'] = r.json()['images'][0]['url']
+
+
+# Creates an empty playlist then adds the user's top songs
+def create_playlist(playlist_details, uri_list):
+    access_token = session.get('token_info').get('access_token')
+    sp = spotipy.Spotify(auth=access_token)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer { access_token }"
+    }
+    username = sp.current_user()['id']
     url = f"https://api.spotify.com/v1/users/{username}/playlists"
     r = requests.post(url, data=playlist_details, headers=headers)
     if r.status_code != 201:
-        main.exceptions(r)
+        exceptions(r)
     else:
         playlist = r.json()
-        add_songs_to_playlist(playlist['id'], uri_list)
+        url = f"https://api.spotify.com/v1/playlists/{playlist['id']}/tracks"
+        uris = json.dumps({'uris': uri_list})
+        req = requests.post(url, data = uris, headers=headers)
+        if req.status_code != 201:
+            exceptions(req)
+        else:
+            print("Playlist successfully created")
         playlist_url = playlist['external_urls']['spotify']
-        embedded_url = main.make_embedded_url(playlist_url)
+        embedded_url = make_embedded_url(playlist_url)
         return embedded_url
 
-def add_songs_to_playlist(playlist_id, uri_list):
-    token_info = spotipy.Spotify(auth=session.get('token_info'))
-    sp = token_info.get('access_token')
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer { sp }"
-    }
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    data = json.dumps({'uris': uri_list})
-    r = requests.post(url, data = data, headers=headers)
-    if r.status_code != 201:
-        main.exceptions(r)
-    else:
-        print("Playlist successfully created")
+
+def remove_punctuation(name):
+    punctuation = """{};:'",<>@#$%^&*_~"""
+    for i in name:
+        if i in punctuation:
+            if i == '&':
+                name = name.replace(i, 'and')
+            elif i == '/' or i == '\\':
+                name = name.replace(i, " ")
+            else:
+                name = name.replace(i, "")
+    return name
+
+
+def make_embedded_url(playlist_url):
+    split_url = playlist_url.partition(".com/")
+    embedded_url = split_url[0] + split_url[1] + 'embed/' + split_url[2] + '?utm_source=generator&theme=0'
+    return embedded_url
+
+
+def exceptions(response):
+    print("Exception occurred with status code: ", response.status_code)
+    print("Error: ", response.text)
+
 
 if __name__ == '__main__':
     app.run()
