965ebd5424ab1f9ec9a873f079875b68393392f8
neilstephen2001 <npar276@aucklanduni.ac.nz>
2023-09-04 23:32:09 +1200

    refactor - home blueprint and utilities

---
+++ app/home/home.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, redirect, url_for, session, render_template
+
+home_blueprint = Blueprint('home_bp', __name__)
+
+
+@home_blueprint.route('/home')
+def index():
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html')
+
+
+@home_blueprint.route('/return-home', methods=['POST'])
+def return_home():
+    return redirect(url_for('index'))
---
+++ app/spotify/utilities.py
@@ -0,0 +1,17 @@
+def remove_punctuation(name: str):
+    punctuation = """{};:'",<>@#$%^&*_~"""
+    for i in name:
+        if i in punctuation:
+            if i == '&':
+                name = name.replace(i, 'and')
+            elif i == '/' or i == '\\':
+                name = name.replace(i, " ")
+            else:
+                name = name.replace(i, "")
+    return name
+
+
+def make_embedded_url(playlist_url: str):
+    split_url = playlist_url.partition(".com/")
+    embedded_url = split_url[0] + split_url[1] + 'embed/' + split_url[2] + '?utm_source=generator&theme=0'
+    return embedded_url
